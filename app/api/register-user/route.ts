b595e5200425f3a244a26ed587200a59e1efe2c0
ClarenceLaria <clarencelaria@gmail.com>
2025-03-13 05:20:03 +0300

    User rerouting updates

--- app/api/register-user/route.ts
+++ app/api/register-user/route.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from 'bcrypt';
-import { error } from "console";
 import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
